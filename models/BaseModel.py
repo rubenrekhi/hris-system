b9d809f283e5a89e9049876ba85ca850480552b6
rubenrekhi <rubenrekhi4@gmail.com>
2025-11-13 15:46:50 -0500

    feat: Create BaseModel class

--- models/BaseModel.py
+++ models/BaseModel.py
@@ -4,3 +4,18 @@ BaseModel.py
 Base model class with common fields and functionality.
 """
 
+from __future__ import annotations
+from uuid import uuid4
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
