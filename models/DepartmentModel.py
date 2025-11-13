8e13e39f009d948adbe46ccba8a941f51b17d9b9
rrekhitenflair <ruben.rekhi@tenflair.com>
2025-11-13 15:58:04 -0500

    feat: Add model for Departments

--- models/DepartmentModel.py
+++ models/DepartmentModel.py
@@ -4,3 +4,16 @@ DepartmentModel.py
 Database model for department organization and management.
 """
 
+from __future__ import annotations
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.BaseModel import BaseModel
+
+class Department(BaseModel):
+    __tablename__ = "departments"
+
+    name = Column(String, nullable=False, unique=True)
+
+    # Backrefs
+    employees = relationship("Employee", back_populates="department")
+    teams = relationship("Team", back_populates="department")
