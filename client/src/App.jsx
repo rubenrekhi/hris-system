b1c668175b9203fc02e8454e521cc3ebbcb3a533
rubenrekhi <rubenrekhi4@gmail.com>
2025-11-17 16:27:37 -0500

    feat: added protected routes to screens, as well as RBAC with nested authorization.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,10 @@ import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import { darkTheme } from './theme/darkTheme';
 import Layout from './components/layout/Layout';
+import ProtectedRoute from './components/ProtectedRoute';
 
 // Import pages
+import Login from './pages/Login';
 import HomePage from './pages/HomePage';
 import EmployeeDirectoryPage from './pages/EmployeeDirectoryPage';
 import OrgChartPage from './pages/OrgChartPage';
@@ -29,25 +31,90 @@ function App() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/employees" element={<EmployeeDirectoryPage />} />
-            <Route path="/org-chart" element={<OrgChartPage />} />
-            <Route path="/audits" element={<AuditsPage />} />
-            <Route path="/teams" element={<TeamsPage />} />
-            <Route path="/reports" element={<ReportsPage />} />
-            <Route path="/reports/directory" element={<DirectoryExportPage />} />
-            <Route path="/reports/org-chart" element={<OrgChartExportPage />} />
-            <Route path="/management" element={<ManagementPage />} />
-            <Route path="/management/bulk-import" element={<BulkImportPage />} />
-            <Route path="/management/create-employee" element={<CreateEmployeePage />} />
-            <Route path="/management/promote-ceo" element={<PromoteToCEOPage />} />
-            <Route path="/management/replace-ceo" element={<ReplaceCEOPage />} />
-            <Route path="/management/create-team" element={<CreateTeamPage />} />
-            <Route path="/management/create-department" element={<CreateDepartmentPage />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          {/* Public route - Login */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected routes - require authentication */}
+          <Route
+            path="/*"
+            element={
+              <ProtectedRoute>
+                <Layout>
+                  <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/dashboard" element={<HomePage />} />
+                    <Route path="/employees" element={<EmployeeDirectoryPage />} />
+                    <Route path="/org-chart" element={<OrgChartPage />} />
+                    <Route path="/audits" element={<AuditsPage />} />
+                    <Route path="/teams" element={<TeamsPage />} />
+                    <Route path="/reports" element={<ReportsPage />} />
+                    <Route path="/reports/directory" element={<DirectoryExportPage />} />
+                    <Route path="/reports/org-chart" element={<OrgChartExportPage />} />
+
+                    {/* Management routes - require hr role or higher (hr and admin can access) */}
+                    <Route
+                      path="/management"
+                      element={
+                        <ProtectedRoute minimumRole="hr">
+                          <ManagementPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/management/bulk-import"
+                      element={
+                        <ProtectedRoute minimumRole="hr">
+                          <BulkImportPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/management/create-employee"
+                      element={
+                        <ProtectedRoute minimumRole="hr">
+                          <CreateEmployeePage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/management/promote-ceo"
+                      element={
+                        <ProtectedRoute minimumRole="admin">
+                          <PromoteToCEOPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/management/replace-ceo"
+                      element={
+                        <ProtectedRoute minimumRole="admin">
+                          <ReplaceCEOPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/management/create-team"
+                      element={
+                        <ProtectedRoute minimumRole="hr">
+                          <CreateTeamPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/management/create-department"
+                      element={
+                        <ProtectedRoute minimumRole="hr">
+                          <CreateDepartmentPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                  </Routes>
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
